ce380a952db3948a7dbc67da2d921db234541061
Sven Anders Robbestad <robbestad@gmail.com>
2020-06-05 12:56:50 +0200

    use process.env.PORT

--- server.js
+++ server.js
@@ -19,6 +19,6 @@ await sharp(infile)
 
 })
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Example app listening at http://localhost:${port}`))
+app.listen(PORT, () => console.log(`Example app listening at http://localhost:${PORT}`))
 
 
