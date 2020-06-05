7947977d1f5a42e4ee05548985747ba863fa846e
Sven Anders Robbestad <robbestad@gmail.com>
2020-06-05 13:07:04 +0200

    fix syntax error

--- server.js
+++ server.js
@@ -2,13 +2,14 @@ const express = require("express");
 const sharp = require('sharp');
 const path = require("path");
 const app = express()
-app.get('/', (req, res) => res.send(`Try: 
+app.get('/', (req, res) => {
+  res.send(`
+  Try:<br/> 
   <a href=\"/image/iguanas/450/230\">/image/iguanas/450/230</a>
   <br/><a href=\"/image/dog/400/300\">/image/dog/400/300</a>
   <br/><a href=\"/image/sheep/500/500\">/image/sheep/500/500</a>
-  <br/><a href=\"/image/corgi/1000/1000\">/image/corgi/1000/1000</a>
-
-`)`
+  <br/><a href=\"/image/corgi/1000/1000\">/image/corgi/1000/1000</a>`)
+})
 app.get("/image/:name/:w/:h", async (req, res) => {
 const {w,h,name} = req.params;
 const file=name+".jpg";
